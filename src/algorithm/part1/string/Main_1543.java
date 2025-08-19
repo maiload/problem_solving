1f8f61c003a8b9848ee0a4429eeb1b9e0ccaa15e
이정진 <jeongjinlee@nhn.com>
2025-08-19 13:17:56 +0900

    Part 1. String
    - 1543 풀이

--- src/algorithm/part1/string/Main_1543.java
+++ src/algorithm/part1/string/Main_1543.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class Main_1543 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        String str = sc.next();
+        int count = 0;
+
+        while (input.indexOf(str) != -1) {
+            int idx = input.indexOf(str);
+            count++;
+            input = input.substring(idx + str.length());
+        }
+        System.out.println(count);
     }
 }
