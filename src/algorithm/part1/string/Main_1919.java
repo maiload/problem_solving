13e5097460ac0809d2d7e6e5400433f28b1b7c7a
이정진 <jeongjinlee@nhn.com>
2025-08-19 13:50:52 +0900

    Part 1. String
    - 1919 풀이

--- src/algorithm/part1/string/Main_1919.java
+++ src/algorithm/part1/string/Main_1919.java
@@ -2,8 +2,22 @@ package algorithm.part1.string;
 
 import java.util.Scanner;
 
+
 public class Main_1919 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        int count = 0;
+        int[] darr1 = str1.chars().distinct().toArray();
+        for (int a : darr1) {
+            int c1 = str1.indexOf((char) a);
+            int c2 = str2.indexOf((char) a);
+
+            System.out.printf("a, c1, c2 : %c, %d, %d\n", a, c1, c2);
+            if (c2 == -1) continue;
+            count = count + Math.min(c1, c2);
+        }
+        System.out.println(str1.length() + str2.length() - (2 * count));
     }
 }
